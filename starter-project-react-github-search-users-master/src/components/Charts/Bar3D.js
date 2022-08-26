f31768bed4fdfccdce27dcd25f1b2ec411b6d859
Vik <svoboda411@gmail.com>
2022-08-26 14:57:45 +0200

    First prep

---
+++ starter-project-react-github-search-users-master/src/components/Charts/Bar3D.js
@@ -0,0 +1,48 @@
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
+
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Column2D from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+// STEP 2 - Chart Data
+
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+
+const ChartComponent = ({data}) => {
+  const chartConfigs = {
+    type: "bar3d", // The chart type
+    width: "700", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption:"Most Forked",
+        yAxisName: "Forks",
+        xAxisName: "Stars",
+        xAxisNameFontSize: "16px",
+        yAxisName: "16px",      },
+      // Chart Data
+      data, 
+    }
+  };
+  return <ReactFC {...chartConfigs} />;
+
+};
+
+export default ChartComponent;
---
+++ starter-project-react-github-search-users-master/src/components/Charts/Doughnut2d.js
@@ -0,0 +1,49 @@
+// STEP 1 - Include Dependencies
+// Include react
+import React from "react";
+
+// Include the react-fusioncharts component
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Column2D from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.candy";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+// STEP 2 - Chart Data
+
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+
+const ChartComponent = ({data}) => {
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption:"Stars per language",
+        decimals: 0,
+        dougnutRadius:"45%",
+        showPercentValues: 0,
+        theme: "candy",
+      },
+      // Chart Data
+      data, 
+    }
+  };
+  return <ReactFC {...chartConfigs} />;
+
+};
+
+export default ChartComponent;
